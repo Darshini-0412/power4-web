7b719d6ce55bf3c5b3b206d787c543537189ed47
ananya <ananyadarshini.pajany@ynov.com>
2025-10-13 10:18:08 +0200

    pwr

---
+++ game.go
@@ -0,0 +1,5 @@
+package game
+
+func NewGame() interface{} {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package main 
+package main
+
+import (
+	"log"
+	"net/http"
+	"html/template"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/play", playHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", httpFileServer(http.Dir("static"))))
+
+	log.Println("Serveur lancé sur http://localhost:")
+}
